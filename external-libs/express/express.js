8f6f6a8be561574d96cfd206e2a2adfb9f8ea7db
Christian Amor Kvalheim <christian.kvalheim@xing.com>
2010-03-11 11:59:19 +0100

    some simple changes while dev

--- external-libs/express/express.js
+++
@@ -1,7 +0,0 @@
-
-require.paths.unshift(__dirname + '/support/js-oo/lib')
-require.paths.unshift(__dirname + '/support/ejs/lib')
-require.paths.unshift(__dirname + '/support/haml/lib')
-require.paths.unshift(__dirname + '/support/sass/lib')
-require('oo')
-require('express/core')
